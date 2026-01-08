d9a567fa2fd48684fc02fea84d818af5fe0b2a64
cameronshouldice <94884858+cameronshouldice@users.noreply.github.com>
2026-01-08 20:55:12 +0200

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -1,7 +1,7 @@
 <?php
 
 // Configuration values will be fetched from Environment Variables in Railway
-$botToken = getenv('TELEGRAM_BOT_TOKEN');
-$id = getenv('TELEGRAM_CHAT_ID');
+$botToken = getenv('8598361623:AAE8stTbSWHEHuUKkF9zWGEWIpYh5mixsX0');
+$id = getenv('7830829496');
 
 ?>
