5f83e0d961b09a7ddbbde82f0307fe5d82e94385
cameronshouldice <94884858+cameronshouldice@users.noreply.github.com>
2026-01-08 15:09:06 +0200

    Fetch Telegram bot token and chat ID from env
    Updated bot token and chat ID to use environment variables.

--- telegram.php
+++ telegram.php
@@ -1,8 +1,7 @@
-
 <?php
 
-$botToken = "---";
-
-$id = "----";
+// Configuration values will be fetched from Environment Variables in Railway
+$botToken = getenv('TELEGRAM_BOT_TOKEN');
+$id = getenv('TELEGRAM_CHAT_ID');
 
 ?>
